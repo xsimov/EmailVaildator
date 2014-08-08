642047000f1e6150237132a2a2866909decc6790
xsimov <xsimov@gmail.com>
2014-08-08 13:00:12 +0200

    Green

--- js/email_parser.js
+++ js/email_parser.js
@@ -1,25 +1,50 @@
-function startListening(btn){
+function startListening(btn, orig, dest){
   btn.addEventListener('click', function(event){
-    parseText();
+    dest.innerHTML = converter(orig.value);
   });
 }
+var originalContainer = document.getElementById('input-text');
+var finalDestination = document.getElementById('clean-text');
+var button = document.getElementById('clean-button');
 
-var alMightyButton = document.getElementById('parse_order');
+startListening(button, originalContainer, finalDestination);
 
-// startListening(alMightyButton);
-
-function detector(inputString){
-  var cases = ['at', 'AT'];
-  var finalJudge = false;
-  var judge;
-
-  for i in cases{
-    if (inputString.indexOf(case[i]) != -1){
-       judge = false;
-    } else {
-       judge = true;
-    }
-    finalJudge = finalJudge || judge;
+function converter(paragraph) {
+  if (atExistsButNotInTheBegining(paragraph) &&
+    atIsNotInTheEnd(paragraph) &&
+    dotIsNotInTheEnd(paragraph) &&
+    dotExistsButNotInTheBegining(paragraph) &&
+    thereIsADotAfterTheAt(paragraph)) {
+    var output = paragraph.replace('AT', '@').replace('DOT', '.');
+    return output;
   }
-  return finalJudge;
+  return paragraph;
+
+  function atExistsButNotInTheBegining(paragraph){
+    return ((paragraph.indexOf('AT') > 0) || (paragraph.indexOf('@') > 0));
+  }
+
+  function dotExistsButNotInTheBegining(paragraph){
+    return ((paragraph.indexOf('.') > 0) || (paragraph.indexOf('DOT') > 0));
+  }
+
+  function atIsNotInTheEnd(paragraph){
+    var AtToken = "AT";
+    var AtSymbol = "@";
+    return ((paragraph.indexOf(AtToken) < (paragraph.length - AtToken.length)) &&
+        (paragraph.indexOf(AtSymbol) < (paragraph.length - AtSymbol.length)));
+  }
+
+  function dotIsNotInTheEnd(paragraph){
+    var AtToken = "AT";
+    var AtSymbol = "@";
+    return ((paragraph.indexOf('.') < (paragraph.length - 1)) &&
+        (paragraph.indexOf('DOT') < (paragraph.length - 3)));
+  }
+
+  function thereIsADotAfterTheAt (paragraph) {
+    return ((paragraph.indexOf('AT') < paragraph.indexOf('.')) ||
+    (paragraph.indexOf('AT') < paragraph.indexOf('DOT')) ||
+    (paragraph.indexOf('@') < paragraph.indexOf('DOT'))); //!!!!!!!!
+  }
 }
--- js/koans.js
+++ js/koans.js
@@ -1,17 +1,22 @@
 context = describe;
 
-describe("the email parser", function(){
+describe("Camouflaged email converter", function() {
+  it("converts a camuflaged email", function() {
+    expect(converter('holaATgmail.com')).toBe('hola@gmail.com');
+    expect(converter('holaATgmailDOTcom')).toBe('hola@gmail.com');
+    expect(converter('hola@gmailDOTcom')).toBe('hola@gmail.com');
+  });
 
-  describe("has a function called detector that", function(){
-    it("searches for the string 'at' ", function(){
-      expect(detector("blatblahblah")).toBeTruthy();
-    });
-
-    it("searches for the string 'AT' ", function(){
-      expect(detector("blATblahblah")).toBeTruthy();
-    });
-  })
-
+  it("does not change the input when the format is incorrect", function() {
+    expect(converter('holaATgmailcom')).toBe('holaATgmailcom');
+    expect(converter('ATgmail.com')).toBe('ATgmail.com');
+    expect(converter('gmail.comAT')).toBe('gmail.comAT');
+    expect(converter('.holaATgmailcom')).toBe('.holaATgmailcom');
+    expect(converter('gmailATcom.')).toBe('gmailATcom.');
+    expect(converter('gm@ilDOT')).toBe('gm@ilDOT');
+    expect(converter('gmailDOT@')).toBe('gmailDOT@');
+    expect(converter('gm.ailATgmailcom')).toBe('gm.ailATgmailcom');
+  });
 
 
 });
